23464faa2f86894a6a8eab542f982f514e948982
Magstruck23 <shashwatpunwatkar@gmail.com>
2026-08-12 12:19:01 +0530

    Update and rename Practical1 to Practical1.sql

---
+++ Practical1.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS List;
+CREATE TABLE List (
+serial_no INT,
+price INT
+);
+INSERT INTO LIST (Serial no, Item, Price)
+VALUES
+(1, 'Driftwood', 1000),
+(2, 'Filter', 550),
+(3, 'Skimmer', 450);
