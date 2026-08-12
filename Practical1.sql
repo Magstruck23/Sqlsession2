d290d8a963f5b6b85bda4e09649b5287ddc15d42
Magstruck23 <shashwatpunwatkar@gmail.com>
2026-08-12 12:43:48 +0530

    Fix SQL syntax and table creation issues

--- Practical1.sql
+++ Practical1.sql
@@ -1,9 +1,10 @@
 DROP TABLE IF EXISTS List;
 CREATE TABLE List (
-serial_no INT,
+  serial_no INT,
+item VARCHAR(20),
 price INT
 );
-INSERT INTO LIST (Serial no, Item, Price)
+INSERT INTO LIST (Serial_no, Item, Price)
 VALUES
 (1, 'Driftwood', 1000),
 (2, 'Filter', 550),
